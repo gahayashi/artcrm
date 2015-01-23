7d87392723e8e211aa51e0464fd8eddee1254d4d
Gabriele Hayashi <gabriele.hayashi@gmail.com>
2015-01-23 00:29:02 -0200

    Starting to add client log information

--- src/artcrm/models/clients.py
+++ src/artcrm/models/clients.py
@@ -3,28 +3,59 @@
 Created on Jan 23, 2015
 @author: Gabriele Hayashi
 '''
+from datetime import datetime
+
+from pyramid.i18n import TranslationStringFactory
 from sqlalchemy.orm.mapper import validates
-from sqlalchemy.sql.schema import Column
-from sqlalchemy.sql.sqltypes import Unicode, String
+from sqlalchemy.sql.schema import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import Unicode, String, Enum, Date, DateTime, \
+    UnicodeText, Boolean
 
 from artcrm import validators
 from artcrm.models import Base, BaseModel
 
 
+_ = TranslationStringFactory('artcrm')
+
 class Client(Base, BaseModel):
     # --- Personal Information ---
     first_name = Column(Unicode(128))
     last_name = Column(Unicode(128))
+    birth = Column(Date, nullable = True)
 
     # --- Contact Information ---
     email = Column(Unicode(120), unique = True, nullable = True)
-    phone = Column(String(len('+99 99 9-9999-9999')), nullable = True)
-    mobile = Column(String(len('+99 99 9-9999-9999')), nullable = True)
 
     @validates('email')
     def validates_email(self, name, email):
         return validators.email(name, email)
 
-    @validates('phone', 'mobile')
+class ClientPhone(Base, BaseModel):
+    MOBILE, HOME, WORK, FAX = 'm', 'h', 'w', 'f'
+    TYPES = {
+        MOBILE: _('mobile'),
+        HOME: _('home'),
+        WORK: _('work'),
+        FAX: _('fax')
+    }
+
+    # --- Relation ---
+    id_client = Column(ForeignKey(Client.id))
+
+    # --- Data ---
+    number = Column(String(len('+99 99 9-9999-9999')), nullable = True)
+    type = Column(Enum(TYPES.keys()))
+    carrier = Column(Unicode(64))
+
+    @validates('number')
     def validates_phones(self, name, phone):
         return validators.phone(name, phone)
+
+class ClientLog(Base, BaseModel):
+    # --- Admin ---
+    timestamp = Column(DateTime, default = datetime.now)
+    is_active = Column(Boolean, default = True)
+
+    # --- Information ---
+    text = Column(UnicodeText)
+    id_client = Column(ForeignKey(Client.id))
